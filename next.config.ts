eedea88689f41ca7162e664dec2ef8930bdcb575
Azzazeru <aaron.fuentesdioca@gmail.com>
2025-06-29 12:32:18 -0400

    fix: correct basePath typo and add static file export step in workflow

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: '/portfolio/',
+  baseBath: '/portfolio',
+  output: 'export',
+  trailingSlash: true
 };
 
 export default nextConfig;
